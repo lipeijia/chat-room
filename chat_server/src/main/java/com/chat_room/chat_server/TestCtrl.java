2d0395410f2acac5712301669bb9cf21a4f76d23
gqsdvb135790@gmail.com <gqsdvb135790@gmail.com>
2023-07-16 16:35:56 +0800

    cross origin

--- chat_server/src/main/java/com/chat_room/chat_server/TestCtrl.java
+++ chat_server/src/main/java/com/chat_room/chat_server/TestCtrl.java
@@ -1,9 +1,11 @@
 package com.chat_room.chat_server;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(value = "http://localhost:3000")
 public class TestCtrl {
     @GetMapping("/hello")
     public String Hello(){
