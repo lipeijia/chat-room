dfe696762a9a60c4a7cdb265200df74b29456dc8
gqsdvb135790@gmail.com <gqsdvb135790@gmail.com>
2023-07-25 13:16:15 +0800

    修改封包格式 example.html

--- chat_server/src/main/java/com/chat_room/chat_server/WebSocketHandler.java
+++ chat_server/src/main/java/com/chat_room/chat_server/WebSocketHandler.java
@@ -16,31 +16,53 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 class Room_guy {
+    public Room_guy(String s, int a){
+        name = s;
+        age = a;
+    }
     @JsonProperty("name")
     public String name;
 
     @JsonProperty("age")
     public int age;
 }
+class Message{
+    public int kind;
+    public String data;
+}
+
 //@RequiredArgsConstructor
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
     public WebSocketHandler(){
 
     }
-    private final List<WebSocketSession> sessionList = new ArrayList<>();
+    private final HashMap<String , WebSocketSession> sessionMap = new HashMap<>();
 
 
-    public List<WebSocketSession> getSessionList() {
-        return sessionList;
+    public HashMap<String , WebSocketSession> getSessionMap() {
+        return sessionMap;
     }
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) {
-        sessionList.add(session);
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception{
+        sessionMap.put(session.getId(), session);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Message m = new Message();
+        m.kind = 1;
+//        m.data = session.getId();
+        Room_guy guys[] = new Room_guy[2];
+        guys[0] = new Room_guy("kk", 11);
+        guys[1] = new Room_guy("cc", 22);
+        String d = objectMapper.writeValueAsString(guys);
+        m.data = d;
+        sessionMap.get(session.getId()).sendMessage(new TextMessage(objectMapper.writeValueAsString(m)));
+
+
         System.out.println("Connection established from " + session.toString() +
                 " @ " + Instant.now().toString());
     }
@@ -61,12 +83,13 @@ public class WebSocketHandler extends TextWebSocketHandler {
         }
         float a = 11;
 
+
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status)
             throws Exception {
-        sessionList.remove(session);
+        sessionMap.remove(session.getId());
         System.out.println("Connection closed by " + session.toString() +
                 " @ " + Instant.now().toString());
     }
